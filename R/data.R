707f9c3c20204b34fe04cdde1a9ba29bf13da529
Dewey Dunnington <dewey@fishandwhistle.net>
2019-05-12 18:38:24 -0300

    add income dataset

--- R/data.R
+++ R/data.R
@@ -1,12 +1,22 @@
 
 #' Canadian University Enrollment
 #'
-#' Canadian University Enrollment by year, province,
-#' program type, credential type, program class, regustration status,
-#' and gender.
+#' Canadian University Enrollment by year, province, program type, credential
+#' type, program class, regustration status, and gender.
 #'
-#' @source
-#' Table: 37-10-0011-01 (formerly: CANSIM 477-0029)
+#' @source Statistics Canada. Table 37-10-0011-01: Postsecondary enrolments,
+#' by program type, credential type, Classification of Instructional Programs,
+#' Primary Grouping (CIP_PG), registration status and sex.
 #' \url{https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=3710001101}
 #'
 "enrollment"
+
+
+#' University Graduate Income
+#'
+#' @source Statistics Canada. Table 37-10-0115-01: Characteristics and median
+#' employment income of longitudinal cohorts of postsecondary graduates two and
+#' five years after graduation, by educational qualification and field of study,
+#' 2010 and 2011 cohorts. \url{https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=3710011501}
+#'
+"income"
---
+++ data-raw/income.R
@@ -0,0 +1,68 @@
+
+library(tidyverse)
+
+# Methodology:
+# Postsecondary Student Information System (PSIS)
+# http://www23.statcan.gc.ca/imdb/p2SV.pl?Function=getSurvey&SDDS=5017
+#
+# Table: 37-10-0115-01
+# https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=3710011501
+
+# # this is a 10 MB download
+# curl::curl_download(
+#   "https://www150.statcan.gc.ca/n1/tbl/csv/37100115-eng.zip",
+#   "data-raw/37100115-eng.zip"
+# )
+# utils::unzip("data-raw/37100115-eng.zip", exdir = "data-raw")
+
+income_raw <- read_csv("data-raw/37100115.csv", na = c("", "NA", "x"))
+
+income_all <- income_raw %>%
+  select(-VECTOR, -COORDINATE) %>%
+  distinct() %>%
+  select(
+    graduate_year = REF_DATE,
+    province = GEO,
+    cred_type = `Educational qualification`,
+    field = `Field of study`,
+    gender = Sex,
+    age_group = `Age group`,
+    immigration_status = `Student status in Canada`,
+    income_group = `Characteristics after graduation`,
+    variable = `Graduate statistics`,
+    value = VALUE
+  )
+
+income <- income_all %>%
+  filter(
+    # no totals for these
+    !str_detect(cred_type, "^Total"),
+    !str_detect(field, "^Total"),
+    !str_detect(gender, "^Total"),
+
+    # only one value for these
+    province == "Canada",
+    age_group == "15 to 64 years",
+    immigration_status == "Canadian and international students",
+    income_group == "Graduates reporting employment income",
+  ) %>%
+  select(-province, -age_group, -immigration_status, -income_group) %>%
+  mutate(
+    variable = fct_recode(
+      variable,
+      "n_students" = "Number of graduates",
+      "income_2yr" = "Median employment income two years after graduation",
+      "income_5yr" = "Median employment income five years after graduation"
+    )
+  ) %>%
+  spread(variable, value) %>%
+  filter(
+    # further restrictions
+    n_students > 15,
+    str_detect(cred_type, "degree")
+  ) %>%
+  mutate(cred_type = str_remove(cred_type, "\\s*degree$"))
+
+# list.files("data-raw", "^37100115", full.names = TRUE) %>% unlink()
+
+usethis::use_data(income, overwrite = TRUE)
